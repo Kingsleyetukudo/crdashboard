ea96e700ed0111a79a77611639ddbb09d4c7e4cd
Kingsley Etukudo <kingsleyj90@gmail.com>
2022-07-21 16:21:41 +0100

    nav update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="nav-col d-flex justify-content-between align-items-center border">
     <div class="search-bar-col">
-        <div class="mobile-bar">
+        <div class="mobile-bar" @click="toggleNav">
             <img src="../assets/bars-solid.svg" class="img-fluid" alt="">
         </div>
 
@@ -29,13 +29,62 @@
         </div>
         <div class="profile-img">
             <img src="../assets/avatar.png" class="img-fluid" alt="">
+            <div class="profile-details">
+                <div class="text border-bottom pb-3 text-end">
+                    <p class="fs-5"> Welcome Admin</p>
+                    <p class="fs-6">Travor James</p>
+                </div>
+  
+                <ul class="pt-3">
+                    <li>My Profile <img src="../assets/user-regular.svg" alt=""></li>
+                    <li>Settings <img src="../assets/gear-solid.svg" alt=""></li>
+                    <li>Logout <img src="../assets/logout.svg" alt=""></li>
+                </ul>
+            </div>
         </div>
+     
     </div>
   </div>
+
+  <div :class="{'mobile-nav-toggle-out' : !showNav, 'mobile-nav-toggle-in' : showNav}">
+   <div class="img-holder  d-flex justify-content-end align-items-center w-100 mb-5">
+     <div class="close-icon" @click="navbarClose">
+        <img src="../assets/xmark-solid.svg" class="img-fluid" alt="">
+    </div>
+   </div>
+
+    <ul>
+        <li> <img src="../assets/house-solid.svg" alt=""> Dashboard</li>
+        <li> <img src="../assets/file-lines-regular.svg" alt=""> Pages</li>
+        <li><img src="../assets/file-lines-regular.svg" alt=""> Applications</li>
+        <li><img src="../assets/file-lines-regular.svg" alt=""> UI Components</li>
+        <li><img src="../assets/file-lines-regular.svg" alt=""> Widgets</li>
+        <li><img src="../assets/file-lines-regular.svg" alt=""> Forms</li>
+        <li><img src="../assets/file-lines-regular.svg" alt=""> Charts</li>
+    </ul>
+  </div>
+
+
+
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            showNav: false
+        }
+    },
+
+    methods: {
+        toggleNav() {
+            this.showNav = !this.showNav
+        },
+
+        navbarClose() {
+            this.showNav = !this.showNav
+        }
+    }
 
 }
 </script>
@@ -55,6 +104,43 @@ export default {
     width: 100%;
 }
 
+.mobile-nav-toggle-in {
+    width: 350px;
+    padding: 2rem;
+    position: absolute;
+    top: 0;
+    border-right: 2px solid red;
+    transition: all .8s ease;
+    overflow: hidden;
+}
+
+.mobile-nav-toggle-out {
+    width: 0px;
+    position: absolute;
+    top: 0;
+    transition: all .8s ease-out;
+    overflow: hidden;
+}
+
+ul {
+    list-style: none;
+    font-size: 1.5em;
+    font-weight: 500;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+}
+
+li {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+}
+
+li img {
+    width: 30px;
+}
+
 .mobile-bar {
     display: none;
 }
@@ -68,6 +154,40 @@ export default {
     padding: .5rem 1rem;
 }
 
+.profile-details {
+    display: none;
+    align-items: flex-end;
+    flex-direction: column;
+    background-color: #4d4f5c;
+    color: #fff;
+    width: 200px;
+    padding: 1rem;
+    position: absolute;
+    right: 0;
+}
+
+.profile-img {
+    position: relative;
+    display: inline-block;
+}
+
+.profile-img:hover  .profile-details{
+    display: block;
+}
+
+.profile-details ul {
+    gap: .8rem;
+    align-items: flex-end;
+}
+
+.profile-details li img {
+    width: 15px;
+}
+
+.profile-details li {
+    font-size: .7em;
+}
+
 input {
     border: none;
     outline: none;
@@ -75,14 +195,15 @@ input {
     color: #fff;
 }
 
-.search_icon {
+.search_icon, .close-icon  {
     width: 25px;
     height: 25px;
 }
 
-.notification, .message, .mobile-bar {
+.notification, .message, .mobile-bar{
     width: 35px;
     height: 35px;
+    cursor: pointer;
 }
 
 .profile-img {
